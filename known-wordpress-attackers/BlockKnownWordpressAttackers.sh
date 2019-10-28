34cd900942122714c7e26e3f0faf1b6b2912cf56
NiPeGun <nipegun@gmail.com>
2019-10-28 11:37:39 +0100

    Update BlockKnownWordpressAttackers.sh

--- known-wordpress-attackers/BlockKnownWordpressAttackers.sh
+++ known-wordpress-attackers/BlockKnownWordpressAttackers.sh
@@ -1,1 +1,15 @@
-.
+#!/bin/bash
+
+# Install NFTables, in case is not installed
+apt-get -y install nftables
+
+# BackUp NFTables original configuration file
+cp /etc/nftables.conf /etc/nftables.conf.bak
+
+# Include the rules in the NFTables configuration file
+sed -i '/^flush ruleset/a include "/root/scripts/net-blocking-tools/known-wordpress-attackers/NFTables.rules"' /etc/nftables.conf
+sed -i -e 's|flush ruleset|flush ruleset\n|g' /etc/nftables.conf
+
+# Reload NFTables rules
+nft --file /etc/nftables.conf
+
