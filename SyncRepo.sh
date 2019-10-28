ed7215e187da573fd50eb3570f5f05a44a451733
NiPeGun <nipegun@gmail.com>
2019-10-28 11:57:44 +0100

    Create SyncRepo.sh

---
+++ SyncRepo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software
+# de dominio público". Puedes hacer lo que te salga de los huevos con él
+# porque es libre de verdad; no libre con condiciones como las licencias GNU
+# y otras patrañas similares. Si se te llena la boca hablando de libertad
+# entonces hazlo realmente libre. No tienes que aceptar ningún tipo de términos
+# de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------
+#  Script to Sync the net-blocking-tools
+#-----------------------------------------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+# Check if there is internet connection before syncing the repo
+wget -q --tries=10 --timeout=20 --spider https://github.com
+  if [[ $? -eq 0 ]]; then
+    echo ""
+    echo -e "  ${ColorVerde}Syncing net-blocking-tools repo...${FinColor}" 
+    echo ""
+    rm /root/scripts/net-blocking-tools -R
+    cd /root/scripts
+    git clone --depth=1 https://github.com/nipegun/net-blocking-tools
+    rm /root/scripts/net-blocking-tools/.git -R
+    rm /root/scripts/net-blocking-tools/README.md
+    chmod +x /root/scripts/net-blocking-tools/*.sh -R
+    echo ""
+    echo -e "${ColorVerde}Repo synced correctly.${FinColor}"
+    echo ""
+  else
+    echo ""
+    echo -e "${ColorRojo}The RepoSync coulden´t start because no internet connection was detected.${FinColor}"
+    echo ""
+fi
