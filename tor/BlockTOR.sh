d5cfb29778992f63ea3c05c0404a105c5ed69c7c
NiPeGun <nipegun@gmail.com>
2019-10-28 18:09:28 +0100

    Update BlockTOR.sh

--- tor/BlockTOR.sh
+++ tor/BlockTOR.sh
@@ -17,8 +17,8 @@ WANIP=$(curl --silent ipinfo.io/ip)
 # Create the NFT sets
 wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$WANIP -O -|sed '/^#/d' |while read IP
   do
-  sed -i '/^define TORNodes.ipv4 = {/a '"$IP"',' /root/scripts/net-blocking-tools/tor/IPv4.nftables
-  sed -i '/^define TORNodes.ipv6 = {/a '"$IP"',' /root/scripts/net-blocking-tools/tor/IPv6.nftables
+    sed -i '/^define TORNodes.ipv4 = {/a '"$IP"',' /root/scripts/net-blocking-tools/tor/IPv4.nftables
+    sed -i '/^define TORNodes.ipv6 = {/a '"$IP"',' /root/scripts/net-blocking-tools/tor/IPv6.nftables
   done
 
 # Delete previous blocks, if exists
@@ -28,7 +28,7 @@ wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$WANIP -O -|s
 cp /etc/nftables.conf /etc/nftables.conf.bak
 
 # Include the rules in the NFTables configuration file
-sed -i '/^flush ruleset/a include "/root/scripts/net-blocking-tools/tor/IPv4.nftables"' /etc/nftables.conf
+sed -i '/^flush ruleset/a include "/root/scripts/net-blocking-tools/tor/NFTables.rules"' /etc/nftables.conf
 sed -i -e 's|flush ruleset|flush ruleset\n|g' /etc/nftables.conf
 
 # Reload NFTables rules
