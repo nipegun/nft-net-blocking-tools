c10899f1e6b3a7af945231ce0cf844bd4a81b79b
NiPeGun <nipegun@gmail.com>
2019-11-06 01:34:32 +0100

    Update CreateIPSets-HAProxy.sh

--- geoipsets/CreateIPSets-HAProxy.sh
+++ geoipsets/CreateIPSets-HAProxy.sh
@@ -9,10 +9,10 @@ find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.subnet
 find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.subnet" -exec sed -i 's| comment | |g' {} \;
 
 # IPv6
-#mkdir -p /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
-#cp /root/scripts/net-blocking-tools/geoipsets/ipset/ipv6/* /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
-#find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.ipv6" -exec bash -c 'mv "$1" "${1%.ipv6}".subnet' - '{}' \;
-#find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i '1d' {} \;
-#find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i 's/^............//' {} \;
-#find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i 's| comment | |g' {} \;
+mkdir -p /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
+cp /root/scripts/net-blocking-tools/geoipsets/ipset/ipv6/* /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.ipv6" -exec bash -c 'mv "$1" "${1%.ipv6}".subnet' - '{}' \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i '1d' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i 's/^............//' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.subnet" -exec sed -i 's| comment | |g' {} \;
 
