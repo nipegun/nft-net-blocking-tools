8192981e19ebe866ba28b368565a47719b0f9ef2
NiPeGun <nipegun@gmail.com>
2019-11-07 02:12:28 +0100

    Update CreateIPSets-HAProxy.sh

--- geoipsets/CreateIPSets-HAProxy.sh
+++ geoipsets/CreateIPSets-HAProxy.sh
@@ -11,18 +11,18 @@ echo ""
 #IPv4
 mkdir -p /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4
 cp /root/scripts/net-blocking-tools/geoipsets/ipset/ipv4/* /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.ipv4" -exec bash -c 'mv "$1" "${1%.ipv4}".acl' - '{}' \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.acl" -exec sed -i '1d' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.acl" -exec sed -i 's/^............//' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.acl" -exec sed -i 's/...........$//' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.acl" -exec sed -i 's| comment | |g' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.ipv4" -exec bash -c 'mv "$1" "${1%.ipv4}".haproxy' - '{}' \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.haproxy" -exec sed -i '1d' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.haproxy" -exec sed -i 's/^............//' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.haproxy" -exec sed -i 's/...........$//' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv4/ -name "*.haproxy" -exec sed -i 's| comment | |g' {} \;
 
 # IPv6
 mkdir -p /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
 cp /root/scripts/net-blocking-tools/geoipsets/ipset/ipv6/* /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.ipv6" -exec bash -c 'mv "$1" "${1%.ipv6}".acl' - '{}' \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.acl" -exec sed -i '1d' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.acl" -exec sed -i 's/^............//' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.acl" -exec sed -i 's/...........$//' {} \;
-find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.acl" -exec sed -i 's| comment | |g' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.ipv6" -exec bash -c 'mv "$1" "${1%.ipv6}".haproxy' - '{}' \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.haproxy" -exec sed -i '1d' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.haproxy" -exec sed -i 's/^............//' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.haproxy" -exec sed -i 's/...........$//' {} \;
+find /root/scripts/net-blocking-tools/geoipsets/haproxyset/ipv6/ -name "*.haproxy" -exec sed -i 's| comment | |g' {} \;
 
