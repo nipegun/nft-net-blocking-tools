f92ccc253fba55d210cdc8819d0db40d8fdc4414
NiPeGun <nipegun@gmail.com>
2019-11-07 02:32:08 +0100

    Update GetNodes.sh

--- tor/GetNodes.sh
+++ tor/GetNodes.sh
@@ -5,12 +5,10 @@ ColorGreen='\033[1;32m'
 EndColor='\033[0m'
 
 echo ""
-echo -e "${ColorGreen}Creating TOR HAProxy sets....${EndColor}"
+echo -e "${ColorGreen}Getting TOR Nodes....${EndColor}"
 echo ""
-
 # Obtain WAN IP of the computer
 WANIP=$(curl --silent ipinfo.io/ip)
-
 # Create the NFTables sets
 wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$WANIP -O -|sed '/^#/d' |while read IP
   do
@@ -18,7 +16,9 @@ wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$WANIP -O -|s
     sed -i '/^define TORNodes.ipv6 = {/a '"$IP"',' /root/scripts/net-blocking-tools/tor/TORNodesIPv6.nftables
   done
 
-# Create de HAProxy set
+echo ""
+echo -e "${ColorGreen}Creating TOR HAProxy sets....${EndColor}"
+echo ""
 cp /root/scripts/net-blocking-tools/tor/TORNodesIPv4.nftables /root/scripts/net-blocking-tools/tor/TORNodesIPv4.haproxy
 cp /root/scripts/net-blocking-tools/tor/TORNodesIPv6.nftables /root/scripts/net-blocking-tools/tor/TORNodesIPv6.haproxy
 find /root/scripts/net-blocking-tools/tor/ -name "*.haproxy" -exec sed -i '1d' {} \;
