d1e3b87ba91fa9662b4baa411ee0ab03d825c225
NiPeGun <nipegun@gmail.com>
2019-11-07 02:21:44 +0100

    Update SyncRepo.sh

--- SyncRepo.sh
+++ SyncRepo.sh
@@ -30,6 +30,7 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
     echo ""
     echo -e "${ColorGreen}Repo synced correctly.${EndColor}"
     echo ""
+    echo ""
     echo -e "${ColorGreen}Creating GeoIP NFTables & IPTables sets....${EndColor}"
     echo ""
     cd /root/scripts/net-blocking-tools/
