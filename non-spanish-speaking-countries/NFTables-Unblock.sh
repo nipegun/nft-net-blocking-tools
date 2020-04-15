7c899bc66d01d9927bfbcc9799f0c158478b7ca5
NiPeGun <nipegun@gmail.com>
2020-04-15 04:18:23 +0200

    Update NFTables-Unblock.sh

--- non-spanish-speaking-countries/NFTables-Unblock.sh
+++ non-spanish-speaking-countries/NFTables-Unblock.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sed -i -e 's|include "/root/scripts/net-blocking-tools/non-spanish-speaking-countries/NFTables.rules"||g' /etc/nftables.conf
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+sed -i -e 's|include "/root/scripts/nft-net-blocking-tools/non-spanish-speaking-countries/NFTables.rules"||g' /etc/nftables.conf
