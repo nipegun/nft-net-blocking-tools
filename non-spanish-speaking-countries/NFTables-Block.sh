efbd22d6b32d6cb5296329ab804bc3a9ae60414b
NiPeGun <nipegun@gmail.com>
2020-04-15 04:19:23 +0200

    Update NFTables-Block.sh

--- non-spanish-speaking-countries/NFTables-Block.sh
+++ non-spanish-speaking-countries/NFTables-Block.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
 ColorRed='\033[1;31m'
 ColorGreen='\033[1;32m'
 ColorEnd='\033[0m'
@@ -12,13 +17,13 @@ echo ""
 apt-get -y install nftables > /dev/null
 
 # Delete previous blocks, if exists
-/root/scripts/net-blocking-tools/non-spanish-speaking-countries/NFTables-Unblock.sh
+/root/scripts/nft-net-blocking-tools/non-spanish-speaking-countries/NFTables-Unblock.sh
 
 # BackUp NFTables original configuration file
 cp /etc/nftables.conf /etc/nftables.conf.bak
 
 # Include the rules in the NFTables configuration file
-sed -i '/^flush ruleset/a include "/root/scripts/net-blocking-tools/non-spanish-speaking-countries/NFTables.rules"' /etc/nftables.conf
+sed -i '/^flush ruleset/a include "/root/scripts/nft-net-blocking-tools/non-spanish-speaking-countries/NFTables.rules"' /etc/nftables.conf
 sed -i -e 's|flush ruleset|flush ruleset\n|g' /etc/nftables.conf
 
 # Reload NFTables rules
