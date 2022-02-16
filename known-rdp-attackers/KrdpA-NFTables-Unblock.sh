70c7dbfb0ca6196a00c8416dc027a9f796fe1e14
NiPeGun <nipegun@gmail.com>
2022-02-16 16:48:11 +0100

    Create KrdpA-NFTables-Unblock.sh

---
+++ known-rdp-attackers/KrdpA-NFTables-Unblock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+sed -i -e 's|include "/root/scripts/nft-net-blocking-tools/known-rdp-attackers/NFTables.rules"||g' /etc/nftables.conf
+
+# Reload NFTables rules
+nft --file /etc/nftables.conf
+
